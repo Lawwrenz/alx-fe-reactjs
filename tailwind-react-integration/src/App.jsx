ae9f0ae76daac80498a91197d69285cd6599fe56
Lawrence Ezealor <lawrenceezealor1@gmail.com>
2025-08-07 11:58:54 +0100

    added UserProfile.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="bg-gradient-to-r from-blue-500 to-purple-600 p-6 rounded-lg shadow-lg max-w-2xl mx-auto text-center">
-  <h1 class="text-3xl font-semibold text-white mb-4">Hello Lawrence</h1>
-  <p class="text-lg text-white/90">Welcome to your first lesson on TailwindCSS</p>
-</div>
+//     <div class="bg-gradient-to-r from-blue-500 to-purple-600 p-6 rounded-lg shadow-lg max-w-2xl mx-auto text-center">
+//   <h1 class="text-3xl font-semibold text-white mb-4">Hello Lawrence</h1>
+//   <p class="text-lg text-white/90">Welcome to your first lesson on TailwindCSS</p>
+// </div>
+<UserProfile />
   )
 }
 
