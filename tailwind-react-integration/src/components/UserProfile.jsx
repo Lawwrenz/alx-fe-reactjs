1225a70df312c0ff1adee075a2798b50621e4e59
Lawrence Ezealor <lawrenceezealor1@gmail.com>
2025-08-07 12:53:48 +0100

    Added advanced interactivity and transitions with tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm sm:max-w-sm mx-auto my-8 sm:my-12 md:my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm sm:max-w-sm mx-auto my-8 sm:my-12 md:my-20 rounded-lg shadow-lg hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
-        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         alt="User"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 mt-4 mb-2 sm:mt-6 sm:mb-3 md:mt-8 md:mb-4">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 mt-4 mb-2 sm:mt-6 sm:mb-3 md:mt-8 md:mb-4 hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-sm sm:text-base text-gray-600">
